52dae771978996281accb2f9eb9e38439e2e5216
ioncrash <ioncrash@gmail.com>
2016-11-30 10:27:28 -0500

    can get to onMedClick
    yay!

--- assets/scripts/meds/ui.js
+++ assets/scripts/meds/ui.js
@@ -2,13 +2,15 @@
 
 const indexMedsTemplate = require('../templates/index-meds.handlebars');
 
-const indexMedsSuccess = (meds) => {
-  $('.med-grid').html(indexMedsTemplate(meds));
+const onMedClick = function(e) {
+  e.preventDefault();
+  console.log('poop');
 };
 
-// const clearBooks = () => {
-//
-// };
+const indexMedsSuccess = (meds) => {
+  $('.med-grid').html(indexMedsTemplate(meds));
+  $('.med-row').on('click', onMedClick);
+};
 
 const failure = (error) => {
   console.error(error);
